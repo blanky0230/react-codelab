5855807714753affb397111476d3eb878b6e663b
thomas.blank <thomas.blank@mayflower.de>
2020-03-09 17:40:07 +0100

    add ReactDom Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
 import "./styles.css";
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Router>
     </div>
   );
 }
